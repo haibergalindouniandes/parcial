ac20335082378905ade04559f57f442f5137e92b
Haiber H. Galindo <h.galindos@uniandes.edu.co>
2022-11-04 00:35:21 -0500

    Se implementa logica para obtener el total de vehiculos por marca.

--- src/app/vehiculo/vehiculo-list/vehiculo-list.component.ts
+++ src/app/vehiculo/vehiculo-list/vehiculo-list.component.ts
@@ -11,13 +11,38 @@ export class VehiculoListComponent implements OnInit {
 
   // Declare variables to use
   vehiculos: Array<Vehiculo> = [];
+  listMarcas: [string, number][] = [];
 
   constructor(private vehiculoService: VehiculoService) { }
 
   getVehiculos(): void {
     this.vehiculoService.getVehiculos().subscribe((vehiculos) => {
       this.vehiculos = vehiculos;
-    })
+      this.getCounMarcas(vehiculos);
+    });
+  }
+
+  getCounMarcas(marcas: Array<Vehiculo>): void {
+    for (let index = 0; index < marcas.length; index++) {
+      let countMarcaVehiculo = 0;
+      let marcaVehiculo = marcas[index].marca;
+      let resultValidate = this.validateExistInArray(marcaVehiculo);
+      if (!resultValidate[0]) {
+        this.listMarcas.push([marcaVehiculo, 1]);
+      }else{
+        countMarcaVehiculo = this.listMarcas[resultValidate[1] as number][1];
+        this.listMarcas[resultValidate[1] as number][1] = countMarcaVehiculo + 1;
+      }
+    }
+  }
+
+  validateExistInArray(valueToSearch: string) {
+    for (let index = 0; index < this.listMarcas.length; index++) {
+      if (this.listMarcas[index][0] == valueToSearch) {
+        return [true, index];
+      }
+    }
+    return [false, 0];
   }
 
   ngOnInit() {
